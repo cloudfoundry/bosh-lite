28d7cd67abf3cbce358c714fa6eef6107f00538b
Benjamin Gandon <benjamin@gandon.org>
2016-03-31 11:23:44 +0200

    Fix misplaced '-y' switch

--- packer/scripts/vbox-guest-additions.sh
+++ packer/scripts/vbox-guest-additions.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-apt-get install -y linux-headers-$(uname -r) build-essential make perl dkms
+apt-get -y install linux-headers-$(uname -r) build-essential make perl dkms
 
 mount -o loop /home/vagrant/VBoxGuestAdditions.iso /media/cdrom
 # Ignore X driver failure, we don't need it
